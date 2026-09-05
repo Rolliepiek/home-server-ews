44d6ee48f6f74b453a7186e428db08b1cbdbb38e
Rolliepiek <ollepierik@protonmail.com>
2026-09-05 16:39:28 +0200

    Update main.rs

--- home-server-ews/src/main.rs
+++ home-server-ews/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+#[test]
+fn test() {
+    setup(true)
+}
+fn setup(test: bool) {
+    let mut input: String = String::new();
+    println!("Welcome to this setup for home-server-ews.");
+    println!("Please answer the following questions:");
+    print!("Is the service that home-server-ews runs on online 24/7? (Yes/No)");
+    if test != true {
+        io::stdin().read_line(&mut input).expect("Error");
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    setup(false);
 }
